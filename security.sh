873c47385c248d2366c6da1649fb03840b40076b
Jens Borch Christiansen <jens.borch@gmail.com>
2019-01-12 16:03:11 +0100

    Created security script

---
+++ security.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# visudo
+# ALL ALL=(ALL) NOPASSWD: /usr/sbin/NetworkManager
+
+# UFW
+pacman -S ufw gufw
+systemctl enable ufw.service
+ufw enable
+# ufw allow from 192.168.0.0/16 to any app SSH
+# ufw allow from 192.168.0.0/16 to any app CIFS
+# ufw allow from 192.168.0.0/16 to any port NFS
+
+echo "auth optional pam_faildelay.so delay=4000000" >> /etc/pam.d/system-login
+echo "auth required pam_tally2.so deny=3 unlock_time=600 onerr=succeed" >> /etc/pam.d/system-login
+echo "account required pam_tally2.so" >> /etc/pam.d/system-login
+
+passwd -l root
+
